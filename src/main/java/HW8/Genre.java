ac7fba45c6c7ca77b2600be1903550fe72676700
Viktor Pakhomov <pahomovvitya@ukr.net>
2021-02-02 23:42:51 +0200

    HW8

---
+++ src/main/java/HW8/Genre.java
@@ -0,0 +1,14 @@
+package HW8;
+
+public enum  Genre {
+    FAIRY_TALE("Fairy tale"),FANTASY("fantasy");
+    String genre;
+
+    Genre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+}
